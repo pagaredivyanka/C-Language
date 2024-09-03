6fca684eb10c90c2aa374eab7bfd7197d5322e2c
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-09-03 10:42:44 +0530

    Add files via upload

---
+++ 00-FirstProgram/Hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+
+int main() {
+    printf("Hello World");
+    int a = 20;
+    int b = 50;
+    int sum = a + b;
+    printf("sum = %d", sum);
+    return 0;
+}
---
+++ 01-Variable/Variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main() {
+    
+    int a = 20;
+    int b = 50;
+    int sum = a + b;
+    printf("sum = %d", sum);
+
+    return 0;
+}
